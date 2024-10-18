e0bb9c28558b4f3844e5b0bf02ddce3976ccf9a7
smile0n <126342426+smile0n@users.noreply.github.com>
2024-10-18 14:56:17 +0100

    Add files via upload

---
+++ pytonconnect.py
@@ -0,0 +1,5 @@
+{
+  "url": "https://github.com/smile0n/swift_bot_data/pytonconnect",
+  "name": "Swift_sms_bot",
+  "iconUrl": "https://raw.githubusercontent.com/XaBbl4/pytonconnect/main/pytonconnect.png"
+}
